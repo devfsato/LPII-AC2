6716a239740af5317f1b8ece7561a359bee13afc
devfsato <fsato@live.com>
2020-03-07 23:57:41 -0300

    'Melhorando_teste'

--- teste.py
+++ teste.py
@@ -1,11 +1,27 @@
+print("\nOlá, Seja Bem Vindo a este Programa que responde algumas funções do seu animal")
+
+print("\nSerá preciso fornecer os dados do seu Camaleão\n")
+
 from ac2_lp2 import Camaleao
-from ac2_lp2 import Gato
 
-c = Camaleao('lulu', 'branco','4','besouro')
+nome = input("Informe o Nome do seu Cameleão: ")
+cor = input("Qual a cor do seu Camaleão: ")
+idade = input("Qual a Idade: ")
+inseto_f = input("Qual o Inseto Favorito:")
+##self, nome, cor, idade, inseto_favorito
 
-print(c.mudar_de_cor())
+c = Camaleao(nome,cor,idade,inseto_f)
 
+print("\nO que gostaria de ver o seu animal fazendo? ")
 
-g = Gato('lulu','cinza','2','s',7)
+ativar = int(input("\nInserir 1 para velo comer e 2 trocar de cor: "))
 
-print(g.morrer())
+#comer_inseto
+#mudar_de_cor
+
+if ativar == 1:
+    print(c.comer_inseto(),'(',inseto_f,')')
+elif cor == 'azul':
+    print(c.mudar_de_cor(),'de',cor,'para VERDE')
+else:
+    print(c.mudar_de_cor(),'de',cor,'para AZUL')
