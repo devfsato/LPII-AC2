531b5bac48296d0341ed7a958c0a0734b4035207
devfsato <fsato@live.com>
2020-03-07 23:03:35 -0300

    'Arquivo_de_Teste_da_new'

---
+++ teste.py
@@ -0,0 +1,11 @@
+from ac2_lp2 import Camaleao
+from ac2_lp2 import Gato
+
+c = Camaleao('lulu', 'branco','4','besouro')
+
+print(c.mudar_de_cor())
+
+
+g = Gato('lulu','cinza','2','s',7)
+
+print(g.morrer())
